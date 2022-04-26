a853161869a7f773d668a8410f62a7f74b50dbbd
iamjnayem <iamj.nayem@gmail.com>
2022-04-26 19:12:49 +0600

    compare string lexicographically

--- 112A.php
+++ 112A.php
@@ -1,1 +1,27 @@
 <?php
+$first_string = strtolower(trim(fgets(STDIN)));
+$second_string = strtolower(trim(fgets(STDIN)));
+
+//echo $first_string . "\n";
+//echo $second_string. "\n";
+
+
+$string_length = strlen($first_string); // strlen($second_string)
+
+$index = 0;
+$flag = true;
+for( ; $index < $string_length; $index++){
+    if($first_string[$index] != $second_string[$index]){
+       $flag = false;
+    }
+    if ($first_string[$index] > $second_string[$index]){
+        echo "1" . "\n";
+        break;
+    }else if($first_string[$index] < $second_string[$index]){
+        echo "-1" . "\n";
+        break;
+    }
+}
+if($index == $string_length && $flag == true){
+    echo "0" . "\n";
+}
